ce68f3f089080924afdcd03fb1759a51346f6eb5
SRIJAN KUMAR KHAN <69808361+srijan-git@users.noreply.github.com>
2022-01-29 17:43:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { GetDataService } from './services/get-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testing';
+  products
+  constructor(private excelService: GetDataService) { }
+
+  ngOnInit(): void {
+    this.excelService.getData().subscribe((data) => {
+      console.log(data);
+      this.products = data
+    })
+  }
+  exportAsExcel() {
+    this.excelService.exportAsExcelFile(this.products, 'Exported_ExcelSheet')
+  }
+
 }
